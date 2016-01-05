b185800eaf3942a19f98b8351425faf92beb2532
Paulo César <paulocesarcs.dev@gmail.com>
2016-01-05 14:04:40 -0200

    Atualizar

--- eventex/settings.py
+++ eventex/settings.py
@@ -101,5 +101,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = 'http://localhost:8001/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

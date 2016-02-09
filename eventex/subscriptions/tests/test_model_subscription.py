72e7711a4029218dd7ed2537ea00c90e663f9ecb
Paulo César <paulocesarcs.dev@gmail.com>
2016-02-09 00:10:28 -0200

    bug models corrigido

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -4,13 +4,13 @@ from eventex.subscriptions.models import Subscription
 
 class SubscriptionModelTest(TestCase):
     def setUp(self):
-       self.obj = Subscription(
-          name='Paulo Cesar',
-          cpf='12345678901',
-          email='paulocesarcs.info@gmail.com',
-          phone='62-94130086'
-       )
-       self.obj.save()
+        self.obj = Subscription(
+           name='Paulo Cesar',
+           cpf='12345678901',
+           email='paulocesarcs.info@gmail.com',
+           phone='62-94130086'
+        )
+        self.obj.save()
 
     def test_create(self):
         self.assertTrue(Subscription.objects.exists())

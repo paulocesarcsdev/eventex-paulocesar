fc4e64620b63770552812f67a2d8e2710a6eae83
Paulo César <paulocesarcs.dev@gmail.com>
2016-02-08 23:53:18 -0200

    Adiconando teste models

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+       self.obj = Subscription(
+          name='Paulo Cesar',
+          cpf='12345678901',
+          email='paulocesarcs.info@gmail.com',
+          phone='62-94130086'
+       )
+       self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())

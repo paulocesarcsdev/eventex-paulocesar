a10dacb0d775dfd18dad9ae325db6ef0600e98f0
Paulo César <paulocesarcs.dev@gmail.com>
2016-02-15 17:48:02 -0200

    update views

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -41,6 +41,7 @@ def detail(request):
                   {'subscription': Subscription()})
 
 
+
 def _send_mail(subject, from_, to, template_name, context):
             body = render_to_string(template_name, context)
             mail.send_mail(subject, body, from_,  [from_, to])
